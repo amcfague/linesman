24ebe9d0514552902310014e9c81a2eed659001f
Andrew McFague <amcfague@wgen.net>
2011-04-11 18:51:41 -0400

    Using dynamic dependencies for Python versions < 2.7

--- setup.py
+++ setup.py
@@ -5,6 +5,15 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
+import sys
+
+install_requires = ["mako", "networkx", "PIL"]
+
+# ordereddict is required for versions < 2.7; its included in collections in
+# versions 2.7+ and 3.0+
+if sys.version_info < (2, 7):
+    install_requires.append("ordereddict")
+
 setup(
     name='linesman',
     version='0.1',
@@ -12,26 +21,21 @@ setup(
     author='Andrew McFague',
     author_email='redmumba@gmail.com',
     url='',
+    zip_safe=False,
     packages=find_packages(exclude=["ez_setup"]),
     package_data = {
         'linesman': ['templates/*', 'media/*'],
     },
-    zip_safe=False,
-    install_requires=[
-        "mako",
-        "networkx",
-        "ordereddict",
-        "PIL"
-    ],
+    install_requires=install_requires,
     classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
     ],
     entry_points="""
         [paste.filter_app_factory]
         profiler = linesman.middleware:profiler_filter_app_factory
-
         [paste.filter_factory]
         profiler = linesman.middleware:profiler_filter_factory
     """
