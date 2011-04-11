281836a811ebcef7f8acb3f36ab10c95a9786272
Andrew McFague <amcfague@wgen.net>
2011-04-11 16:54:51 -0400

    "Liensman" sounds like a super-hero banker.

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Liensman'
+project = u'Linesman'
 copyright = u'2011, Wireless Generation'
 
 # The version info for the project you're documenting, acts as replacement for
