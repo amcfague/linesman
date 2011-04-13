416dec214a4f44da6c718ae4d93c9e61d72548de
Calen Pennington <cpennington@wgen.net>
2011-04-13 11:39:22 -0400

    Remove annoy bump when clicking a leaf

--- linesman/media/js/accordian.js
+++ linesman/media/js/accordian.js
@@ -3,6 +3,9 @@ $(function(){
     $('#callhierarchy .row').click(function() {
         // navigate up to the nearest parent list item
         var parent_li = $(this).parent('li');
+        if (parent_li.hasClass('leaf')) {
+            return false;
+        }
         parent_li.children('ul').toggle(slideSpeed);
         parent_li.toggleClass('open closed');
     });
