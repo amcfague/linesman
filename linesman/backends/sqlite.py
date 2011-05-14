28acfb90443f889bab9b6be21200dfcbe6d4a3b0
Andrew McFague <amcfague@wgen.net>
2011-05-14 10:28:44 -0400

    Adding GPL header to relevant source files

--- linesman/backends/sqlite.py
+++ linesman/backends/sqlite.py
@@ -1,3 +1,19 @@
+# This file is part of linesman.
+#
+# linesman is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# linesman is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along
+# with linesman.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import logging
 import sqlite3
 import time
